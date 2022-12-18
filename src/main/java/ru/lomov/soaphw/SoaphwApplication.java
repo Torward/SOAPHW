46d5764e4919ef13718ed8b3b6faaa39e0bee75e
Torward <lomovfg@gmail.com>
2022-12-18 13:59:16 +1000

    Initial commit

---
+++ src/main/java/ru/lomov/soaphw/SoaphwApplication.java
@@ -0,0 +1,13 @@
+package ru.lomov.soaphw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaphwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoaphwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/lomov/soaphw/SoaphwApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lomov.soaphw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaphwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
